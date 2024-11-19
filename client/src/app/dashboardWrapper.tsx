70beef969aec1a9faf6915731ae7e6407ad4aad3
Yiorgos <yiorgosdev@gmail.com>
2024-11-19 17:28:21 +0100

    add: initial structure

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/app/components/Navbar";
+
+export default function DashboardWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* sidebar */}
+      sidebar
+      <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  )
+}
