eeb6822556ab771e5f7f93439e775c52df7a55ba
Yiorgos <yiorgosdev@gmail.com>
2025-02-01 18:59:25 +0100

    add: components | Header, Modal, ModalNewTask

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+type Props = {
+  name: string
+  buttonComponent?: any
+  isSmallText?: boolean
+}
+
+export default function Header({ name, buttonComponent, isSmallText=false }: Props) {
+  return (
+    <div className='mb-5 flex w-full items-center justify-between'>
+      <h1 className={`${isSmallText ? 'text-lg' : 'text-2xl'} font-semibold dark:text-white`}>
+        {name}
+      </h1>
+      {buttonComponent}
+    </div>
+  )
+}
