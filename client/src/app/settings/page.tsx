0d187771487fa9406e0654dc6144eabccd9518ab
Yiorgos <yiorgosdev@gmail.com>
2025-02-05 17:22:27 +0100

    add: static user details page

---
+++ client/src/app/settings/page.tsx
@@ -0,0 +1,37 @@
+import Header from '@/components/Header'
+
+export default function Settings() {
+  const userSettings = {
+    userName: 'johndoe',
+    email: 'john.doe@example.com',
+    teamName: 'Team Name',
+    roleName: 'Role'
+  }
+
+  const labelStyles = 'block text-sm font-medium dark:text-white'
+  const textStyles = 'mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2 dark:text-white'
+
+  return (
+    <div className='p-8'>
+      <Header name='Settings' />
+      <div className='space-y-4'>
+        <div>
+          <label className={labelStyles}>Username</label>
+          <div className={textStyles}>{userSettings.userName}</div>
+        </div>
+        <div>
+          <label className={labelStyles}>Email</label>
+          <div className={textStyles}>{userSettings.email}</div>
+        </div>
+        <div>
+          <label className={labelStyles}>Team</label>
+          <div className={textStyles}>{userSettings.teamName}</div>
+        </div>
+        <div>
+          <label className={labelStyles}>Role</label>
+          <div className={textStyles}>{userSettings.roleName}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
