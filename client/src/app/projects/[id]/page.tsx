e8cce98ec114c8ecdc335f13b83e9fb06f449820
Yiorgos <yiorgosdev@gmail.com>
2025-02-05 15:54:19 +0100

    add: project views

---
+++ client/src/app/projects/[id]/page.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useState } from "react"
+import ProjectHeader from "@/app/projects/ProjectHeader"
+import Board from "../BoardView"
+import List from "../ListView"
+import Timeline from "../TimelineView"
+import Table from "../TableView"
+import ModalNewTask from "@/components/ModalNewTask"
+
+type Props = {
+  params: { id: string }
+}
+
+export default function Project({ params }: Props) {
+  const { id } = params
+  const [activeTab, setActiveTab] = useState('Board')
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false)
+
+  return (
+    <div>
+      <ModalNewTask
+        id={id}
+        isOpen={isModalNewTaskOpen}
+        onClose={() => setIsModalNewTaskOpen(false)}
+      />
+      <ProjectHeader 
+        activeTab={activeTab} 
+        setActiveTab={setActiveTab} 
+      />
+      {activeTab === 'Board' && (
+        <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === 'List' && (
+        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === 'Timeline' && (
+        <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === 'Table' && (
+        <Table id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+    </div>
+  )
+}
--- client/src/app/redux.tsx
+++ client/src/app/redux.tsx
@@ -1,16 +1,15 @@
-'use client'
-
 import { useRef } from "react";
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import {
+  TypedUseSelectorHook,
   useDispatch,
   useSelector,
   Provider,
-} from 'react-redux'
-import { TypedUseSelectorHook } from "react-redux";
-import globalReducer from "./state";
-import { api } from "./state/api";
-import { setupListeners } from '@reduxjs/toolkit/query'
+} from "react-redux";
+import globalReducer from "@/state";
+import { api } from "@/state/api";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
 import {
   persistStore,
   persistReducer,
@@ -20,72 +19,73 @@ import {
   PERSIST,
   PURGE,
   REGISTER,
-} from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
-import createWebStorage from 'redux-persist/lib/storage/createWebStorage'
+} from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 
-/* Redux Persistance */
-function createNoopStorage() {
+/* REDUX PERSISTENCE */
+const createNoopStorage = () => {
   return {
     getItem(_key: any) {
-      return Promise.resolve(null)
+      return Promise.resolve(null);
     },
     setItem(_key: any, value: any) {
-      return Promise.resolve(value)
+      return Promise.resolve(value);
     },
     removeItem(_key: any) {
-      return Promise.resolve()
-    }
-  }
-}
+      return Promise.resolve();
+    },
+  };
+};
 
-const storage = typeof window == 'undefined'
-  ? createNoopStorage()
-  : createWebStorage('local')
+const storage =
+  typeof window === "undefined"
+    ? createNoopStorage()
+    : createWebStorage("local");
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ['global']
-}
-
+  whitelist: ["global"],
+};
 const rootReducer = combineReducers({
   global: globalReducer,
   [api.reducerPath]: api.reducer,
-})
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-/* Redux Store */
-export function makeStore() {
+/* REDUX STORE */
+export const makeStore = () => {
   return configureStore({
     reducer: persistedReducer,
     middleware: (getDefault) =>
       getDefault({
         serializableCheck: {
-          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
         },
-      }).concat(api.middleware)
-  })
-}
+      }).concat(api.middleware),
+  });
+};
 
-/* Redux Types */
-export type AppStore = ReturnType<typeof makeStore>
-export type RootState = ReturnType<AppStore['getState']>
-export type AppDispatch = AppStore['dispatch']
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+/* REDUX TYPES */
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
-/* Provider */
-export default function StoreProvider({ children }: { children: React.ReactNode }) {
-  const storeRef = useRef<AppStore | null>(null)
-
+/* PROVIDER */
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const storeRef = useRef<AppStore>();
   if (!storeRef.current) {
-    storeRef.current = makeStore()
-    setupListeners(storeRef.current.dispatch)
+    storeRef.current = makeStore();
+    setupListeners(storeRef.current.dispatch);
   }
-
-  const persistor = persistStore(storeRef.current)
+  const persistor = persistStore(storeRef.current);
 
   return (
     <Provider store={storeRef.current}>
@@ -93,5 +93,5 @@ export default function StoreProvider({ children }: { children: React.ReactNode 
         {children}
       </PersistGate>
     </Provider>
-  )
+  );
 }
